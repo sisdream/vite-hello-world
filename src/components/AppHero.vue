af974990e68de9e163ebda6af94e6c8194c6518e
sisdream <sissipascale2@gmail.com>
2024-02-07 16:21:25 +0100

    feat: first vue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+      </div>
+</template>
+<script></script>
+<style>
+    
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+<script></script>
+<style></style>
